8569535ba99f2fb089d69463152e9215b7a193ae
LeslieLlama <leslieundinecrooks@gmail.com>
2025-01-18 12:30:52 +1300

    Rudimentary Score calc
    made a function that can detect coins in sequence for score calculations, and gave coins a base score value. Also changed how flips and re-flips were displayed to be more accurate, and pencilled out UI

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -4,8 +4,14 @@ var rng = RandomNumberGenerator.new()
 var coinCount = 0
 var reflipCount = 3
 
+var headsValue = 50
+var tailsValue = 50
+var currentScore = 0
+
 var CoinHistory = []
 var CoinHistorySprites = []
+var scoreDisplayed : bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$HeadsCoinSprite.hide()
@@ -18,8 +24,14 @@ func _process(delta: float) -> void:
 
 
 func _on_next_coin_button_button_up() -> void:
-	if coinCount == 6:
+	if scoreDisplayed == true:
 		reset_table()
+		return
+	if coinCount == 6:
+		display_score()
+		scoreDisplayed = true
+		$ReDoCoinButton.disabled = true	
+		$NextCoinButton.text = "Next Round"
 	else:
 		flip_coin(false)
 	
@@ -27,7 +39,6 @@ func _on_re_do_coin_button_button_up() -> void:
 	flip_coin(true)
 	
 func flip_coin(is_reflip : bool):
-
 	var flip_value = rng.randi_range(0, 1)
 	if flip_value == 1:
 		$HeadsCoinSprite.show()
@@ -35,33 +46,67 @@ func flip_coin(is_reflip : bool):
 		if is_reflip == true && reflipCount > 0:
 			CoinHistory.pop_back()
 			CoinHistory.append(1)
-			CoinHistorySprites[coinCount].color = Color("D06559")
 			reflipCount -= 1
 		else:
 			CoinHistory.append(1)
-			CoinHistorySprites[coinCount].color = Color("D06559")
 			coinCount += 1
-		
 	else: 
 		$HeadsCoinSprite.hide()
 		$TailsCoinSprite.show()
 		if is_reflip == true && reflipCount > 0:
 			CoinHistory.pop_back()
 			CoinHistory.append(0)
-			CoinHistorySprites[coinCount].color = Color("65A7C1")
 			reflipCount -= 1
 		else:
 			CoinHistory.append(0)
-			CoinHistorySprites[coinCount].color = Color("65A7C1")
 			coinCount += 1
 	$ReDoCoinButton/ReflipAmmount.text = str(reflipCount,"x")
 	$NextCoinButton/CoinAmmount.text = str(coinCount,"/6")
+	print(CoinHistory)
+	coin_history_display_update()
+	if coinCount == 6: $NextCoinButton.text = "Score Coins"
 	
+
+func coin_history_display_update():
+	for c in CoinHistory:
+		if c == 1:
+			CoinHistorySprites[coinCount-1].color = Color("D06559")
+		else:
+			CoinHistorySprites[coinCount-1].color = Color("65A7C1")
+			
+func coin_pattern_searcher():
+	var runCount = 1
+	var previousCoinValue = -1
+	var highestRunCount = 1
+	for c in CoinHistory:
+		if c == 0:
+			currentScore += tailsValue
+		else: 
+			currentScore += headsValue
+		if c == previousCoinValue:
+			runCount += 1
+		else: 
+			runCount = 1
+		previousCoinValue = c
+		if runCount >= 3:
+			highestRunCount = runCount
+	currentScore *= highestRunCount
+	
+		
+func display_score():
+	coin_pattern_searcher()
+	$RoundScoreLabel.text = str(currentScore)
+	$ShopPanel.show()
 func reset_table():
+	currentScore = 0
 	CoinHistory.clear()
 	for c in CoinHistorySprites:
 		c.color = Color.WHITE
 	coinCount = 0
 	reflipCount = 3
+	scoreDisplayed = false
 	$ReDoCoinButton/ReflipAmmount.text = str(reflipCount,"x")
 	$NextCoinButton/CoinAmmount.text = str(coinCount,"/6")
+	$NextCoinButton.text = "Flip"
+	$ReDoCoinButton.disabled = false
+	$ShopPanel.hide()
