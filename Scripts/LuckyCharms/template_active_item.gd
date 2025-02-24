9834fce42b89aadb9fa07f295d98487c93cdb5d9
LeslieLlama <leslieundinecrooks@gmail.com>
2025-02-24 13:20:53 +1300

    Added active item template
    Created an inherited script from lucky charm for Active items, items which activate an effect when clicked.

---
+++ Scripts/LuckyCharms/template_active_item.gd
@@ -0,0 +1,59 @@
+extends LuckyCharm
+
+@export var charges = 0
+@export var maxCharges = 0
+var is_useable : bool = false
+func _ready() -> void:
+	charges = maxCharges
+	_update_charge_counter()
+	Signals.ResetTable.connect(_reset_table)
+	Signals.ScoreCoins.connect(_score_coins)
+
+func _on_mouse_entered() -> void:
+	Signals.emit_signal("Mouse_Over", Name, Description)
+
+func _on_mouse_exited() -> void:
+	if not Rect2(Vector2(), self.size).has_point(get_viewport().get_mouse_position()):
+		Signals.emit_signal("Mouse_End")
+		
+func _reset_table():
+	if is_enabled == true:
+		_ready_animation()
+		charges = maxCharges
+		_update_charge_counter()
+		is_useable = true
+	
+func _score_coins():
+	is_useable = false
+	if tween:
+		tween.kill()
+	
+func _update_charge_counter():
+	$ChargeCounter.text = str("🗲",charges)
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT :
+			mouseHold = true
+	if event is InputEventMouseButton and event.pressed == false:
+		if event.button_index == MOUSE_BUTTON_LEFT :
+			if charges > 0 and is_useable == true:
+				_active_use()
+			mouseHold = false
+			
+			
+func _active_use():
+	if is_enabled == false:
+		return
+	if is_useable == false:
+		return
+	print("item activated")
+	_activation_animation()
+	charges -= 1
+	_update_charge_counter()
+func _ready_animation():
+	if tween:
+		tween.kill()
+	tween = get_tree().create_tween().bind_node(self).set_loops()
+	tween.tween_property($TextureRect, "rotation_degrees", -5, 1).set_trans(Tween.TRANS_SINE)
+	tween.chain().tween_property($TextureRect, "rotation_degrees", 5, 1).set_trans(Tween.TRANS_SINE)
