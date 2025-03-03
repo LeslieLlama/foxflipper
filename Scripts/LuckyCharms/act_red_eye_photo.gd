db8e2739ccdafc52a4f4c612c7d184e3082c1229
LeslieLlama <leslieundinecrooks@gmail.com>
2025-03-03 22:58:12 +1300

    Added new item : Red Eye Photo
    Added a new item, the red eye photo, which returns all coins and clears history, basically resetting the round for if you get a bad roll

--- Scripts/LuckyCharms/act_red_eye_photo.gd
+++ Scripts/LuckyCharms/act_red_eye_photo.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	_update_charge_counter()
 	Signals.ResetTable.connect(_reset_table)
 	Signals.ScoreCoins.connect(_score_coins)
-	#Signals.FlippedCoin.connect(_coin_flipped)
+	Signals.FlippedCoin.connect(_coin_flipped)
 	
 func _active_use():
 	if is_enabled == false:
@@ -16,16 +16,20 @@ func _active_use():
 		return
 	print("item activated")
 	
-	#clear all coins 
+	Signals.emit_signal("ResetTable")
+	Globals.currentScore = 0
+	Globals.CoinHistory.clear()
+	Globals.coinCount = 0
+	Globals.coinsToThrow = Globals.maxCoinCount
 	
 	_activation_animation()
 	charges -= 1
 	_update_charge_counter()
 	
-#func _coin_flipped(coinCount):
-	#if coinCount >= 1 and charges > 0:
-		#_ready_animation()
-		#is_useable = true
+func _coin_flipped(coinCount):
+	if coinCount >= 1 and charges > 0:
+		_ready_animation()
+		is_useable = true
 	
 func _reset_table():
 	if is_enabled == true:
@@ -36,5 +40,5 @@ func _reset_table():
 func _update_charge_counter():
 	$ChargeCounter.text = str("🗲",charges)
 	Description = str(
-		"Click this item to reflip the last flipped coin, ",charges," times per round"
+		"Click this charm to return all coins and clear coin history (once per round)"
 	)
