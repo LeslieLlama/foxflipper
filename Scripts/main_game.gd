61e0239e41cc65674aa4613de18f19040f8d32d4
LeslieLlama <leslieundinecrooks@gmail.com>
2025-04-11 20:09:06 +1200

    Tutorial Setup
    Setting up UI for a tutorial or how-to manual screen.

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -284,3 +284,6 @@ func reset_table():
 
 func _on_item_compendium_button_toggled(toggled_on: bool) -> void:
 	$ItemCompendium.visible = toggled_on
+
+func _on_help_button_toggled(toggled_on: bool) -> void:
+	$HowToPlay.visible = toggled_on
